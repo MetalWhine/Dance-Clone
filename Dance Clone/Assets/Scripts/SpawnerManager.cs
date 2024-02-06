d66fcee6117ef482c7fa7f3e6b9113d31330c950
Nathanael Setiawan <82701665+MetalWhine@users.noreply.github.com>
2024-02-06 18:52:58 +1100

    Track and notes initial push
    ##Technical Overview##
    - Added track and notes model
    - Added legs
    - Created a new scene
    - Update GitIgnore to finally ignore large files
    ##Current Issues##
    - Timing design, have not found solution for proper timing yet
    - Sliding notes will be very difficult
    
    ##Fixes/Plans##
    - Adding proper notes
    - Using basic rythm game design to implement to VR

---
+++ Dance Clone/Assets/Scripts/SpawnerManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerManager : MonoBehaviour
+{
+    [Header("Song settings")]
+    [SerializeField]
+    private float BPM = 100.0f;
+}
